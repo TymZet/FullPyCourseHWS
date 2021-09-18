61c5152cb1c1e475a40f9b77e5e4d5ab94dfe081
TymZet <87326172+TymZet@users.noreply.github.com>
2021-09-18 11:36:05 +0300

    Update Ex2(countvalues).py

--- Ex2(countvalues).py
+++ Ex2(countvalues).py
@@ -1,16 +1,1 @@
-d = {"1": "3", "5": 0, "8": "3", 56: "54", "847": "54"}
-l = []
-d1 = {}
-for i in d.values():
-    d1[i] = 0
-    l.append(i)
 
-unique = []
-for i in l:
-    if i in unique:
-        d1[i] += 1
-    else:
-        unique.append(i)
-        d1[i] += 1
-
-print(d1)
